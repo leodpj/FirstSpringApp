a43928992b37605f1563f96e0119366b58507ef0
leodpj <leodj@gmail.com>
2025-07-01 20:46:05 +0100

    update

--- src/main/java/com/finsolutions/first_string_app/controller/HelloWolrdController.java
+++ src/main/java/com/finsolutions/first_string_app/controller/HelloWolrdController.java
@@ -1,5 +1,12 @@
 package com.finsolutions.first_string_app.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+@ResponseBody
 public class HelloWolrdController {
 }
